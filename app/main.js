3a8b59daed93e16a9897e246a8098c23ab06c9b4
dragon <dragonzhu1988@126.com>
2017-08-21 09:15:13 +0700

    start

---
+++ app/main.js
@@ -0,0 +1,5 @@
+/**
+ * Created by dragon on 2017/8/17.
+ */
+const zdc = require('./zdc.js');
+document.querySelector("#root").appendChild(zdc());
---
+++ app/zdc.js
@@ -0,0 +1,8 @@
+/**
+ * Created by dragon on 2017/8/17.
+ */
+module.exports = function() {
+    var zdc = document.createElement('div');
+    zdc.textContent = "Hi there and greetings!";
+    return zdc;
+};
